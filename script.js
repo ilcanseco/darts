4a58e8326310361021a308e7717f8e608ba735ee
Ivan Canseco <ilcanseco97@gmail.com>
2020-07-03 06:54:23 -0400

    Created base message in the DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+let container = document.getElementById("container");
+
+let message = document.createElement("div");
+message.textContent = "Select a game";
+message.style.fontSize = "3vh";
+container.appendChild(message);
