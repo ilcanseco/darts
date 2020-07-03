86b53b7710378ddb3aa5b5d71d5c188da41fa59d
Ivan Canseco <ilcanseco97@gmail.com>
2020-07-03 10:39:59 -0400

    Added the rest of the functions to make the game

--- script.js
+++ script.js
@@ -63,6 +63,10 @@ function makeTable(players) {
   container.appendChild(scoreBox);
 
   inProgress = true;
+
+  /* Have to figure this out still */
+  gameSwitch(currentGame);
+  /* Have to figure this out still */
 }
 
 function clearTable() {
@@ -81,7 +85,7 @@ function makeFiveOhOne() {
   inProgress = false;
   clearMessage();
   message.textContent =
-    "Count down from 501. Final dart must be a double to win. Enter the turn total in the box. Click start for a New Game";
+    "Count down from 501. Final dart must be a double to win. Enter the turn total in the box. Click start for a New Game.";
 
   currentGame = "501";
   start();
@@ -91,8 +95,73 @@ function makeOneThousandOne() {
   inProgress = false;
   clearMessage();
   message.textContent =
-    "Count down from 1001. Final Dart must be a double to win. Enter the turn total in the box. Click start for a New Game";
+    "Count down from 1001. Final Dart must be a double to win. Enter the turn total in the box. Click start for a New Game.";
 
   currentGame = "1001";
+  start();
+}
+
+function makeCricket() {
+  inProgress = false;
+  clearMessage();
+  message.textContent =
+    "The objective is to clear all houses. To do this, a total of 3 must be crossed off per house. If you clear a house and your" +
+    " opponent does not have that house cleared yet, you can score points the same amount as that house. If you have all houses cleared" +
+    " first, you win. If you have less points than your opponent and all houses are cleared, you must score more or equal points to win." +
+    " Click start for a New Game.";
+
+  currentGame = "cricket";
+  /*    DONT
+   *     FORGET
+   *     THIS
+   *     SHIT */
+}
+
+function makeAtw() {
+  inProgress = false;
+  clearMessage();
+  message.textContent =
+    "All players start with 100 points. Follow along with the messages to see what to aim for. If all darts miss, player total score " +
+    "gets halved. If any of the darts hit, the total gets added to your score. Highest score at the end of the game wins. Enter the " +
+    "turn total in the box, or 0 if all darts miss. Click start for a New Game.";
+
+  currentGame = "Atw";
+  start();
+}
+
+function makeEightFifty() {
+  inProgress = false;
+  clearMessage();
+  message.textContent =
+    "On each turn the player aims for sections on the dart board from 1 to 20. If any of the darts hit the total gets added to the score, " +
+    "but only if they scored in the corresponding number. For example if the target number is 7 and only one dart hit 7, then the total " +
+    "for that turn would only be 7 points. All doubles and triples are ignored except for the D25. Click the button options for how many " +
+    "darts scored. Click start for a New Game.";
+
+  currentGame = "850";
+  start();
+}
+
+function makeBlindShot() {
+  inProgress = false;
+  clearMessage();
+  message.textContent =
+    "The player's first throw is completely random. The subsequent throws should be within a 2 cm radius of the first throw. The player " +
+    "scores points based on how many darts are within close proximity, with maximum 2 points per turn. If the first throw is off the " +
+    "board, the player gets a rethrow. The first player to 20 points wins. Click the button options for how many points scored. " +
+    "Click start for a New Game.";
+
+  currentGame = "blind";
+  start();
+}
+
+function makeFinisher() {
+  inProgress = false;
+  clearMessage();
+  message.textContent =
+    "01 Game finishing practice. Players start with a score of 170 and count down until score reaches 0. Final dart must be a double. " +
+    "Enter the turn total in the box. Click start for a New Game.";
+
+  currentGame = "finisher";
   start();
 }
